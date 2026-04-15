58c2afa79f736922a12d0e73ee2707ebc10c71c8
salmondio <sergej.serzanin1@gmail.com>
2026-04-15 15:53:06 +0500

    Разработанная практическая работа на оценку удовлетворительно Serzhanin

--- app/src/main/java/com/example/practic_2/MainActivity.java
+++ app/src/main/java/com/example/practic_2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.practic_2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private String _name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +27,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStepAge(View vew){
+        EditText name = findViewById(R.id.editTextText);
+        _name = name.getText().toString();
+
         setContentView(R.layout.step_2);
+
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("Привет, очень приятно познакомиться!\nДрузья меня называют шлюхой, и ты можешь!\n Сколько тебе лет, "+_name+"?");
     }
 
     public void onStepSex(View view){
         setContentView(R.layout.step_3);
+
+        TextView tv = findViewById(R.id.textView);
+        tv.setText(_name+", подскажи, пожалуйста,\nУ тебя есть пипися?");
     }
 
     public void onStepCard(View view){
         setContentView(R.layout.step_4);
+
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("Отлично! остался один моментик.\n"+_name+", напиши ниже номер своей банковсткой карты\nи 3 цифры на её обороте<3");
     }
 }
